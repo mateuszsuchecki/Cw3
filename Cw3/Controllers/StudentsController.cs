de90377783b303c842eb255daee127ea79e5549a
Mateusz Suchecki <s19130@pjwstk.edu.pl>
2020-03-24 20:31:38 +0100

    init

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using Cw3.DAL;
 using Cw3.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace Cw3.Controllers
 {
@@ -20,16 +24,64 @@ namespace Cw3.Controllers
         [HttpGet]
         public IActionResult GetStudents(string orderBy)
         {
-            return Ok(_dbService.GetStudents());
+            var list = new List<Student>();
+            using (var client = new SqlConnection("Data Source=db-mssql;Initial Catalog=pgago; Integrated Security=True"))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = client;
+
+                command.CommandText = "select * from Student";
+                client.Open();
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(new Student() 
+                    { 
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"].ToString(),
+                        IndexNumber = reader["IndexNumber"].ToString(),
+                        BirthDate = DateTime.Parse(reader["BirthDate"].ToString())
+
+                    });
+                }
+            }
+
+            return Ok(list);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetStudent(int id)
+        public IActionResult GetStudent(string id)
         {
-            if (id == 1) return Ok("Kowalski");
-            else if (id == 2) return Ok("Malewski");
+            var list = new List<Object>();
+            using (var client = new SqlConnection("Data Source=db-mssql;Initial Catalog=pgago; Integrated Security=True"))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = client;
 
-            return NotFound("Nie znaleziono studenta");
+                command.CommandText =   "select e.Semester Semester, " +
+                                        "st.Name StudiesName from Student s" +
+                                        "inner join Enrollment e on s.IdEnrollment=e.IdEnrollment" +
+                                        "inner join Studies st on e.IdStudy=st.IdStudy"+
+                                        $"where s.IndexNumber=@id";
+                command.Parameters.AddWithValue("id", id);
+
+
+                client.Open();
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var studia = new
+                    {
+                        Semestr = reader["Semester"].ToString(),
+                        StudiesName = reader["StudiesName"].ToString()
+                    };
+                    list.Add(studia);
+                }
+            }
+
+            return Ok(list);
         }
 
         [HttpPost]
